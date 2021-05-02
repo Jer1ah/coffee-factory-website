4e127141e86e42c26d7e7c329ae3d9e4cc4751b2
Jeriah Bowers <jeriah97bowers@yahoo.com>
2021-05-02 18:56:31 -0400

    working on navigation scroll functionality

--- js/index.js
+++ js/index.js
@@ -3,14 +3,31 @@ const navigationController = (() => {
     const _navigation = document.querySelector('.navigation');
 
     window.addEventListener('scroll', () => {
-        if(window.scrollY > 150) {
+
+        if(window.innerWidth > 724) {
+            if(window.scrollY > 150) {
+                _navigation.style.position = "fixed";
+                _navigation.style.backgroundColor = "#120f0f";
+                _navigation.style.border = "none";
+            } else {
+                _navigation.style.position = "inherit";
+                _navigation.style.backgroundColor = "inherit";
+                _navigation.style.borderBottom = ".01rem solid rgba(255,255,255,.25)";
+            }
+        }
+
+        if(window.innerWidth <= 725) {
             _navigation.style.position = "fixed";
+        } 
+    });
+
+    window.addEventListener('resize', () => {
+        if(window.innerWidth <= 725) {
             _navigation.style.backgroundColor = "#120f0f";
-            _navigation.style.border = "none";
+            _navigation.style.borderBottom = "none";
         } else {
-            _navigation.style.position = "inherit";
-            _navigation.style.backgroundColor = "inherit";
             _navigation.style.borderBottom = ".01rem solid rgba(255,255,255,.25)";
+            _navigation.style.backgroundColor = "inherit";
         }
     });
 
